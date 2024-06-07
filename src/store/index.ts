65fc8b8c51bd47f39a3b12a55db6cc1ad47034e4
SupuniAbeysinghe <supuniab1@gmail.com>
2024-06-07 23:52:31 +0530

    redux toolkit setup created

---
+++ src/store/index.ts
@@ -0,0 +1,24 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const initialState : {
+  board: string[];
+  boardSize: number;
+}={
+  board: [],
+  boardSize:8,
+};
+const candyCrushSlice = createSlice({
+  name: "CandyCrush",
+  initialState,
+  reducers:{
+
+  }
+})
+
+export const store = configureStore({
+  reducer: {
+    candyCruch:candyCrushSlice.reducer,
+  },
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
